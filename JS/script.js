72c0aee29cb76e9c23e4fd51f9024147e6322b33
Hale <agerard57@protonmail.com>
2020-05-24 00:56:09 +0200

    HTML, CSS and JS for the icons

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,12 @@ function affProfile() {
       x.style.display = "none";
     }
   } 
+
+  function affMenu() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
