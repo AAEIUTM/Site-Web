e6f7734e229c2fa6ca77aeebd2182cdc53e545eb
Leknif571 <gsfproject2018@gmail.com>
2020-05-24 23:37:55 +0200

    a j'ai modif bg

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,21 @@
 function affProfile() {
     var x = document.getElementById("textCo");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
-  } 
+}
 
-  function affMenu() {
+function affMenu() {
     var x = document.getElementById("menu");
+    var element = document.getElementById("contenu");
     if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  } 
+        x.style.display = "block";
 
+        element.classList.toggle("menufermer");
+    }
+} else {
+    x.style.display = "none";
+}
+}
