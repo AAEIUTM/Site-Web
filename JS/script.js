5e521025b0e7da52699ecebbd533ed94d64a8ab2
GERARD Alexandre <56207146+agerard57@users.noreply.github.com>
2020-05-25 23:39:18 +0200

    v.05.25.23

--- JS/script.js
+++ JS/script.js
@@ -28,3 +28,29 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
+
+function affMenuHTML() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        document.getElementById("fleche").src = "../medias/images/fleche1.png"
+        document.getElementById("contenu").className = "contenuouvert"
+    } else {
+        x.style.display = "none";
+        document.getElementById("fleche").src = "../medias/images/fleche.png"
+        document.getElementById("contenu").className = "contenufermer"
+    }
+}
+
+
+function affMenuHTMLinscr() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        document.getElementById("fleche").src = "../medias/images/fleche1.png"
+    } else {
+        x.style.display = "none";
+        document.getElementById("fleche").src = "../medias/images/fleche.png"
+    }
+
+}
