6fff2b1f33b5b90748b27674d029eb2ebe687c99
GERARD Alexandre <56207146+agerard57@users.noreply.github.com>
2020-05-25 08:59:13 +0200

    Added chat box

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,25 @@
 function affProfile() {
     var x = document.getElementById("textCo");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
-  } 
+}
 
-  function affMenu() {
+function affMenu() {
     var x = document.getElementById("menu");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
-  } 
+}
 
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
